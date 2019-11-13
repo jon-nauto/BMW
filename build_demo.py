0a72069a217228acbe6b693fd70532f9830a59a2
Jon Emrich <jon.emrich@nauto.com>
2019-11-12 16:51:32 -0800

    making file paths relative

--- build_demo.py
+++ build_demo.py
@@ -10,33 +10,33 @@ import subprocess
 import os
 
 def main():
-    v_path = 'C:/Users/AlonzoWilkins/Desktop/code/BMW/visualization.py'
+    v_path = 'visualization.py'
     if os.path.exists(v_path):
         os.unlink(v_path)
         print('Deleting file at path: %s' % v_path)
-    utils_path = 'C:/Users/AlonzoWilkins/Desktop/code/BMW/utils.py'
+    utils_path = 'utils.py'
     if os.path.exists(utils_path):
         os.unlink(utils_path)
         print('Deleting file at path: %s' % utils_path)
-    config_path = 'C:/Users/AlonzoWilkins/Desktop/code/BMW/dist/config.json'
+    config_path = 'dist/config.json'
     if os.path.exists(config_path):
         os.unlink(config_path)
         print('Deleting file at path: %s' % config_path)
-    videos_path = 'C:/Users/AlonzoWilkins/Desktop/code/BMW/dist/videos'
+    videos_path = 'dist/videos'
     if os.path.exists(videos_path):
         shutil.rmtree(videos_path)
         print('Deleting folder at path: %s' % videos_path)             
     print('Copying visualization.py')
-    shutil.copy('C:/Users/AlonzoWilkins/Desktop/code/nauto-ai/models/distraction/keras/bmw-demo/visualization.py','C:/Users/AlonzoWilkins/Desktop/code/BMW')
+    shutil.copy('../nauto-ai/models/distraction/keras/bmw-demo/visualization.py','.')
     print('Copying utils.py')
-    shutil.copy('C:/Users/AlonzoWilkins/Desktop/code/nauto-ai/models/distraction/keras/bmw-demo/utils.py','C:/Users/AlonzoWilkins/Desktop/code/BMW')
+    shutil.copy('../nauto-ai/models/distraction/keras/bmw-demo/utils.py','.')
     print('Building EXE')
     cmd = ['pyinstaller','--onefile','BMWDemo_setup.spec']
     subprocess.Popen(cmd,shell=True).wait()
     print('Copying videos')
-    shutil.copytree('C:/Users/AlonzoWilkins/Desktop/code/BMW/videos', 'C:/Users/AlonzoWilkins/Desktop/code/BMW/dist/videos')
+    shutil.copytree('videos', 'dist/videos')
     print('Copying config.json')
-    shutil.copy('C:/Users/AlonzoWilkins/Desktop/code/BMW/config.json','C:/Users/AlonzoWilkins/Desktop/code/BMW/dist/config.json')
+    shutil.copy('config.json','dist/config.json')
     print('Zipping directory')
     shutil.make_archive('zip/BMWDemo','zip','dist')
     print('Complete')
